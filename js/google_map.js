cf17ff60244fef6621f0ec92924e5339460317c2
Adrian <team.adrian@gmail.com>
2018-10-15 21:50:23 -0700

    Added schedule; added extra check for the map part

--- js/google_map.js
+++ js/google_map.js
@@ -35,6 +35,10 @@ function init() {
 
         for (var x = 0; x < addresses.length; x++) {
             $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+                if (data.status === 'OVER_QUERY_LIMIT') {
+                    return;
+                }
+
                 var p = data.results[0].geometry.location
                 var latlng = new google.maps.LatLng(p.lat, p.lng);
                 new google.maps.Marker({
